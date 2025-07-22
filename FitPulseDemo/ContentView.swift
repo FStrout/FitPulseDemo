d51ca922debd9fda22cf01114937366945f468a9
Fred Strout <37194417+FStrout@users.noreply.github.com>
2025-07-22 10:59:34 -0500

    Quick commit to validate the removal of LocalKeys from tracking.

--- FitPulseDemo/ContentView.swift
+++ FitPulseDemo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("It's a wonderful day in the neighborhood!")
         }
         .padding()
     }
