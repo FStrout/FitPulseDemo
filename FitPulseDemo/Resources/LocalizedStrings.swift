7aef6ce18e61a266fde842fe898243d9214ba087
Fred Strout <37194417+FStrout@users.noreply.github.com>
2025-07-22 11:02:47 -0500

    Created a LocalizedStrings object.

---
+++ FitPulseDemo/Resources/LocalizedStrings.swift
@@ -0,0 +1,12 @@
+//
+//  LocalizedStrings.swift
+//  FitPulseDemo
+//
+//  Created by Fred Strout on 7/22/25.
+//
+
+import Foundation
+
+struct LocalizedStrings {
+  static let title = "FitPulse Demo"
+}
