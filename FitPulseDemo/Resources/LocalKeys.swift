612d932c198a8bb50c93e10ba546c0580a374144
Fred Strout <37194417+FStrout@users.noreply.github.com>
2025-07-22 10:35:28 -0500

    Added LocalKeys file.
    Created a LocalKeys file and added it to the GitIgnore.

---
+++ FitPulseDemo/Resources/LocalKeys.swift
@@ -0,0 +1,12 @@
+//
+//  LocalKeys.swift
+//  FitPulseDemo
+//
+//  Created by Fred Strout on 7/22/25.
+//
+
+import Foundation
+
+struct LocalKeys {
+  static let SmartSpectraApiKey: String = "FitPulseDemoUserDefaults"
+}
