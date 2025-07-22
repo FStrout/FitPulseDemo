859ae551118dbf14a85f803de06897c9911d9efb
Fred Strout <37194417+FStrout@users.noreply.github.com>
2025-07-22 09:45:29 -0500

    Project Initialization
    Initial commit encompassing the project creation.

---
+++ FitPulseDemo/FitPulseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPulseDemoApp.swift
+//  FitPulseDemo
+//
+//  Created by Fred Strout on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FitPulseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
